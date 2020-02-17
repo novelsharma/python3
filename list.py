fe580504cf94e304fe480ef7f958e1ee4da238b7
novelsharma <novelsharma018@gmail.com>
2020-02-18 01:40:49 +0600

    many code

---
+++ list.py
@@ -0,0 +1,44 @@
+# str = "hello world"
+
+# lst = ['hello',[1,2,3,[4,5,6]]]
+
+# print(str[0])
+
+# x = lst[1][3]
+# print(x[2])
+
+# lst2 = [5,10]
+
+# lst2[1] = 20
+
+# lst2.append(30)
+# lst2.insert(0,40)
+# lst2.insert(2,50)
+# lst2.pop()
+# lst2.remove(50)
+
+# print(lst2.index(20))
+
+# a=2
+# b=3
+# add=a+b
+# print("Add: {0}".format(add))
+# print(f"Add: {add} ")
+# sentance="i am here."
+# sentance1=sentance.replace('here','there')
+#print(sentance1)
+
+# """name="novel sharma raj"
+# print(name.find('raj'))"""
+# schoolName="feni computer institute"
+# replace=schoolName.replace('feni','dhaka')
+# print(replace)
+
+# school='feni computer institute in feni'
+# schoolName=school.replace('feni','dhaka')
+# print(schoolName)
+
+# x="dhaka"
+# y="feni"
+# z="mirpur"
+# print(x + "|" + y + "|" + z )
